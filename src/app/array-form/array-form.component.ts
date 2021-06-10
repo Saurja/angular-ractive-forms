ec6f7ffca7750f0ee69da80d1bda7adc86696723
Saurja Ghosh <saurjaghoshofficial@gmail.com>
2021-06-10 09:40:55 +0530

    Valid Form Complete

--- src/app/array-form/array-form.component.ts
+++ src/app/array-form/array-form.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 @Component({
   selector: 'app-array-form',
   templateUrl: './array-form.component.html',
-  styleUrls: ['./array-form.component.css']
+  styleUrls: ['./array-form.component.css'],
 })
 export class ArrayFormComponent implements OnInit {
-
-  constructor() { }
+  myForm!: FormGroup;
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.myForm = this.fb.group({
+      email: '',
+      phones: this.fb.array([]),
+    });
   }
 
+  get phoneForms() {
+    return this.myForm.get('phones') as FormArray;
+  }
+
+  addPhone() {
+    const phone = this.fb.group({
+      area: [],
+      prefix: [],
+      line: [],
+    });
+
+    this.phoneForms.push(phone);
+  }
+
+  deletePhone(i: number) {
+    this.phoneForms.removeAt(i);
+  }
 }
--- src/app/valid-form/valid-form.component.ts
+++ src/app/valid-form/valid-form.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-valid-form',
   templateUrl: './valid-form.component.html',
-  styleUrls: ['./valid-form.component.css']
+  styleUrls: ['./valid-form.component.css'],
 })
 export class ValidFormComponent implements OnInit {
-
-  constructor() { }
+  myForm!: FormGroup;
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.myForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$'
+          ),
+        ],
+      ],
+      age: [
+        null,
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.min(18),
+          Validators.max(65),
+        ],
+      ],
+      agree: [false, Validators.requiredTrue],
+    });
   }
 
+  get email() {
+    return this.myForm.get('email');
+  }
+  get password() {
+    return this.myForm.get('password');
+  }
+  get age() {
+    return this.myForm.get('age');
+  }
+  get agree() {
+    return this.myForm.get('agree');
+  }
 }
