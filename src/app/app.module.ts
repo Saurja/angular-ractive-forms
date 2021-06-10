734fa5cbe8609c0585ac65bf805ff616239fc01d
Saurja Ghosh <saurjaghoshofficial@gmail.com>
2021-06-10 08:04:08 +0530

    Basic Form Complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,41 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ArrayFormComponent } from './array-form/array-form.component';
+import { BasicFormComponent } from './basic-form/basic-form.component';
+import { NestedFormComponent } from './nested-form/nested-form.component';
+import { SubmitFormComponent } from './submit-form/submit-form.component';
+import { ValidFormComponent } from './valid-form/valid-form.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArrayFormComponent,
+    BasicFormComponent,
+    NestedFormComponent,
+    SubmitFormComponent,
+    ValidFormComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatChipsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
